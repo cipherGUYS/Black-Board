d5e179aeb3a5799d09dafe736ff10b501f57ca8d
Tomin Joy <tominjk007@gmail.com>
2022-10-28 10:17:14 +0530

    chore: backend added

---
+++ README copy.md
@@ -0,0 +1,3 @@
+# Black-Board
+<hr>
+A web-app to assist students in vewing presentations in real time without latency
---
+++ index.js
@@ -0,0 +1,57 @@
+const express_ = require('express');
+const app =express_();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+app.use(express_.static(__dirname+'/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect',()=>{
+        console.log("disconnect");
+    });
+    socket.on('user joined',(props)=>{
+            io.emit('user joined',props);
+    });
+    socket.on("chat-message",(msg,isurl)=>{
+        const as_url=msg.match(urlRegex,msg);
+        const url=`<a href="${as_url}" style="color:'blue'">${as_url}</a>`;
+        msg=msg.replace(as_url,url);
+        console.log(msg);
+                io.emit("chat-message",msg);
+    })
+    socket.on('create-room',(id)=>{
+       
+        room = 10;
+        socket.to(room).emit("newmessage",'hello from room !')
+        app.get('/room/:id',(req,res)=>{
+            console.log(req.params.id);
+            res.sendFile(__dirname+'/index.html')
+        })
+        
+    })
+    socket.on('join-room',(id)=>{
+        console.log(`user joined  at id ${id}`)
+        room = id;
+        
+        app.get('/joinroom/:props',(req,res)=>{
+            console.log(req.params.props);
+            res.sendFile(__dirname+`/Room.html`)
+        })
+        socket.join(id);
+        socket.on("newmessage",()=>{
+            io.to(room).emit("newmessage","hello this from room")
+        })
+        
+    })
+
+
+})
+
+const PORT = process.env.PORT || 3002 
+http.listen(PORT,()=>{
+    console.log("started server at ",PORT);
+})
